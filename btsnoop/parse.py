e385113396b904c4be8961e94f160f553123f4fa
martind <martind@mapfactor.com>
2014-10-31 21:47:16 +0100

    found (?) corresponding command to AT*PCMD (AR Drone2)

--- btsnoop/parse.py
+++ btsnoop/parse.py
@@ -32,7 +32,6 @@ def parseBTSnoop( filename ):
         # bit 1 ... 0 = data, 1 = command/event
         if startTime is None:
             startTime = time64
-#        print flags, ((time64-startTime)/1000)/1000.
         data = f.read(origLen)
         assert len(data) == origLen, (len(data), origLen)
         if flags == 0:
@@ -42,7 +41,14 @@ def parseBTSnoop( filename ):
             assert tmp[3] in [0x9, 0xa, 0xb,0xd, 0x11, 0x16, 0x18, 0x1a, 0x1b] , hex(tmp[3])
             assert len(tmp)-5 == tmp[3], (len(tmp), tmp[3])
             assert tmp[4] == 0, tmp[4]
-            print [hex(x) for x in tmp[5:]]
+            assert len(tmp)-9 == tmp[5], (len(tmp), tmp[5])
+            assert tmp[6] == 0, tmp[6]
+#            print flags, ((time64-startTime)/1000)/1000.
+#            print [hex(x) for x in tmp[5:]]
+            if tmp[5] == 0x12:
+                # looks like it is similar to AR Drone2 AT*PCMD
+                assert tmp[5:5+8] == [0x12, 0x0, 0x4, 0x0, 0x52, 0x40, 0x0, 0x2]
+                print struct.unpack("=BHHBhhf", data[5+8:])
         i += 1
     print "Records", i
 
