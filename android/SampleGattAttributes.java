2d8eacf9c4f0d7a040d994c2c6f0fbef8dcf44fa
martind <martind@mapfactor.com>
2014-11-08 20:45:15 +0100

    Complete list of used UUIDs (unknown meaning)

--- android/SampleGattAttributes.java
+++ android/SampleGattAttributes.java
@@ -30,6 +30,8 @@ public class SampleGattAttributes {
         // Sample Services.
         attributes.put("0000180d-0000-1000-8000-00805f9b34fb", "Heart Rate Service");
         attributes.put("0000180a-0000-1000-8000-00805f9b34fb", "Device Information Service");
+        attributes.put("00001800-0000-1000-8000-00805f9b34fb", "Service GAP_UUID");
+        attributes.put("00001801-0000-1000-8000-00805f9b34fb", "Service GATT_UUID");
         // Sample Characteristics.
         attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
         attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
@@ -37,6 +39,26 @@ public class SampleGattAttributes {
         attributes.put("00002a01-0000-1000-8000-00805f9b34fb", "Appearance");
         attributes.put("00002a04-0000-1000-8000-00805f9b34fb", "Peripheral Preferred Connection Parameters");
         attributes.put("00002a05-0000-1000-8000-00805f9b34fb", "Service Changed");
+
+        // Parrot specific Services
+        attributes.put("9a66fa00-0800-9191-11e4-012d1540cb8e", "Parrot Service - A00");
+        attributes.put("9a66fb00-0800-9191-11e4-012d1540cb8e", "Parrot Service - B00");
+        attributes.put("9a66fc00-0800-9191-11e4-012d1540cb8e", "Parrot Service - C00");
+        attributes.put("9a66fd21-0800-9191-11e4-012d1540cb8e", "Parrot Service - D21");
+        attributes.put("9a66fd51-0800-9191-11e4-012d1540cb8e", "Parrot Service - D51");
+
+        // Parrot minidrone Rolling Spider specific characteristics
+        attributes.put("9a66fa01-0800-9191-11e4-012d1540cb8e", "Parrot - A01"); // + complete range to A1F
+        attributes.put("9a66fa1f-0800-9191-11e4-012d1540cb8e", "Parrot - A1F");
+        attributes.put("9a66fb01-0800-9191-11e4-012d1540cb8e", "Parrot - B01"); // + complete range to B1F
+        attributes.put("9a66fb1f-0800-9191-11e4-012d1540cb8e", "Parrot - B1F");
+        attributes.put("9a66ffc1-0800-9191-11e4-012d1540cb8e", "Parrot - FC1");
+        attributes.put("9a66fd22-0800-9191-11e4-012d1540cb8e", "Parrot - D22");
+        attributes.put("9a66fd23-0800-9191-11e4-012d1540cb8e", "Parrot - D23");
+        attributes.put("9a66fd24-0800-9191-11e4-012d1540cb8e", "Parrot - D24");
+        attributes.put("9a66fd52-0800-9191-11e4-012d1540cb8e", "Parrot - D52");
+        attributes.put("9a66fd53-0800-9191-11e4-012d1540cb8e", "Parrot - D53");
+        attributes.put("9a66fd54-0800-9191-11e4-012d1540cb8e", "Parrot - D54");
     }
 
     public static String lookup(String uuid, String defaultName) {
