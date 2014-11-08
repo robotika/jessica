2a4b8219e468e2bb6f2f6de0bd2c36c05b32d627
martind <martind@mapfactor.com>
2014-11-08 19:58:26 +0100

    added public characteristics

--- android/SampleGattAttributes.java
+++ android/SampleGattAttributes.java
@@ -33,6 +33,10 @@ public class SampleGattAttributes {
         // Sample Characteristics.
         attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
         attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
+        attributes.put("00002a00-0000-1000-8000-00805f9b34fb", "Device Name");
+        attributes.put("00002a01-0000-1000-8000-00805f9b34fb", "Appearance");
+        attributes.put("00002a04-0000-1000-8000-00805f9b34fb", "Peripheral Preferred Connection Parameters");
+        attributes.put("00002a05-0000-1000-8000-00805f9b34fb", "Service Changed");
     }
 
     public static String lookup(String uuid, String defaultName) {
