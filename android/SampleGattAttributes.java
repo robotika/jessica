876eb50506b149734d6527ca2504313ccc618113
martind <martind@mapfactor.com>
2014-11-12 03:51:19 +0100

    decoded handles for Bxx and Date/Time characteristics

--- android/SampleGattAttributes.java
+++ android/SampleGattAttributes.java
@@ -42,7 +42,7 @@ public class SampleGattAttributes {
 
         // Parrot specific Services
         attributes.put("9a66fa00-0800-9191-11e4-012d1540cb8e", "Parrot Service - A00");
-        attributes.put("9a66fb00-0800-9191-11e4-012d1540cb8e", "Parrot Service - B00");
+        attributes.put("9a66fb00-0800-9191-11e4-012d1540cb8e", "Parrot Service - B00"); // handle 0x92
         attributes.put("9a66fc00-0800-9191-11e4-012d1540cb8e", "Parrot Service - C00");
         attributes.put("9a66fd21-0800-9191-11e4-012d1540cb8e", "Parrot Service - D21");
         attributes.put("9a66fd51-0800-9191-11e4-012d1540cb8e", "Parrot Service - D51");
@@ -50,10 +50,11 @@ public class SampleGattAttributes {
         // Parrot minidrone Rolling Spider specific characteristics
         attributes.put("9a66fa01-0800-9191-11e4-012d1540cb8e", "Parrot - A01"); // + complete range to A1F (handle 0x22 - 0x7F)
         attributes.put("9a66fa0a-0800-9191-11e4-012d1540cb8e", "Parrot - Power Motors");     // handle 0x40
-        attributes.put("9a66fa0b-0800-9191-11e4-012d1540cb8e", "Parrot - set numbers T?");   // handle 0x43
+        attributes.put("9a66fa0b-0800-9191-11e4-012d1540cb8e", "Parrot - Date/Time");   // handle 0x43, 1:2014-10-29, 2:T223238+0100, 3:0x0
+
         attributes.put("9a66fa1e-0800-9191-11e4-012d1540cb8e", "Parrot - init count 1..20"); // handle 0x7C
         attributes.put("9a66fa1f-0800-9191-11e4-012d1540cb8e", "Parrot - A1F");              // handle 0x7F
-        attributes.put("9a66fb01-0800-9191-11e4-012d1540cb8e", "Parrot - B01"); // + complete range to B1F
+        attributes.put("9a66fb01-0800-9191-11e4-012d1540cb8e", "Parrot - B01"); // + complete range to B1F (handle 0x92-0xEF)
         attributes.put("9a66fb1f-0800-9191-11e4-012d1540cb8e", "Parrot - B1F");
         attributes.put("9a66ffc1-0800-9191-11e4-012d1540cb8e", "Parrot - FC1"); // handle 0x102
         attributes.put("9a66fd22-0800-9191-11e4-012d1540cb8e", "Parrot - D22"); // handle 0x112
